31a07961ae0af461909e770bce304408703eabf0
Luca Pizzoferrato <Luca.Pizzoferrato@Outlook.com>
2020-11-09 14:30:26 -0500

    updated index.html and script.js

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,10 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var charsCharacters = " !#$%^&*()_+=<>?,.~`'";
+  var lowCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numNumbers = "0123456789";
 
   passwordText.value = password;
 
