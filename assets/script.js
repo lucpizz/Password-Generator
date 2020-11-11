290a1e3056a4fadfca6cc30d41f49d05ee0afa77
Luca Pizzoferrato <Luca.Pizzoferrato@Outlook.com>
2020-11-11 14:29:26 -0500

    updated index.html and script.js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,27 @@
 // Assignment Code
+var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*\(\)_-=+\<\>?;:\"\'\'\{\}\[\]";
+var charRange = document.getElementById("#charRange");
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var numbersRange = "";
-  var characters = " !#$%^&*()_+=<>?,.~`'";
-  var lowCase = "abcdefghijklmnopqrstuvwxyz"
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-
-  const char = document.getElementById('characters');
-  const lCase = document.getElementById('lowCaseLetters');
-  const uCase = document.getElementById('upperCaseLetters');
-  const num = document.getElementById('numbers');
 
   passwordText.value = password;
+
+}
+
+function generatePassword() {
+
+var pword = "";
+
+for (var i = 0; i<charRange; i++ ) {
+
+  pword += characters.charAt(Math.floor(Math.random() * characters.length));
+}
+  return pword;
 
 }
 
